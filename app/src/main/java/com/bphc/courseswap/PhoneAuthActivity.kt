0ce4162bb86100c4821b9fdaf33c76593c7c7c37
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-21 23:22:05 +0530

    Added internet permission

---
+++ app/src/main/java/com/bphc/courseswap/PhoneAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.bphc.courseswap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_auth)
+    }
+}

b3e2a940e4d6c18aee70336a68fb3777a6ef4ff3
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-24 09:50:06 +0530

    User enters assigned and desired course

--- app/src/main/java/com/bphc/courseswap/fragments/MakeSwapRequest.kt
+++ app/src/main/java/com/bphc/courseswap/fragments/MakeSwapRequest.kt
@@ -5,21 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.bphc.courseswap.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+import com.bphc.courseswap.firebase.Auth
+import com.bphc.courseswap.models.User
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.fragment_make_swap_request.*
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MakeSwapRequest.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MakeSwapRequest : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var user: User
+
+    private lateinit var inputAssignedCourseNumber: String
+    private lateinit var inputAssignedCourseName: String
+    private lateinit var inputDesiredCourseNumber: String
+    private lateinit var inputDesiredCourseName: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,28 +35,79 @@ class MakeSwapRequest : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
         return inflater.inflate(R.layout.fragment_make_swap_request, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        assigned_course_number.requestFocus()
+        make_request_button.setOnClickListener {
+            if (!validateFields())
+                return@setOnClickListener
+
+            user = User(
+                Auth.userEmail,
+                Auth.userPhoneNumber,
+                "$inputAssignedCourseNumber $inputAssignedCourseName",
+                "$inputDesiredCourseNumber $inputDesiredCourseName"
+            )
+            Toast.makeText(context, user.toString(), Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun validateFields(): Boolean{
+
+        inputAssignedCourseNumber = assigned_course_number.editText?.text.toString().trim()
+        inputAssignedCourseName = assigned_course_name.editText?.text.toString().trim()
+        inputDesiredCourseNumber = desired_course_number.editText?.text.toString().trim()
+        inputDesiredCourseName = desired_course_name.editText?.text.toString().trim()
+
+        if (inputAssignedCourseNumber.isEmpty()) {
+            assigned_course_number.error = "Please type in your assigned course number"
+            return false
+        } else {
+            assigned_course_number.error = null
+        }
+
+        if (inputAssignedCourseName.isEmpty()) {
+            assigned_course_name.error = "Please type in your assigned course name"
+            return false
+        } else {
+            assigned_course_name.error = null
+        }
+
+        if (inputDesiredCourseNumber.isEmpty()) {
+            desired_course_number.error = "Please type in your desired course number"
+            return false
+        } else {
+            desired_course_number.error = null
+        }
+
+        if (inputDesiredCourseName.isEmpty()) {
+            desired_course_name.error = "Please type in your desired course name"
+            return false
+        } else {
+            desired_course_name.error = null
+        }
+        return true
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MakeSwapRequest.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                MakeSwapRequest().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MakeSwapRequest().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }

8befdcae783a46403a2638dc8d97e1ac063533ca
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-24 09:51:16 +0530

    Obkect

--- app/src/main/java/com/bphc/courseswap/firebase/Auth.kt
+++ app/src/main/java/com/bphc/courseswap/firebase/Auth.kt
@@ -1,4 +1,18 @@
 package com.bphc.courseswap.firebase
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 object Auth {
+
+    lateinit var userEmail: String
+    lateinit var userPhoneNumber: String
+
+    fun auth() : FirebaseAuth {
+        return Firebase.auth
+    }
+
+
+
 }

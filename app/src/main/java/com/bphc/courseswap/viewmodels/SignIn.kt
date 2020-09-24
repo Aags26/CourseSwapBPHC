acf587cdf0d25497b345efdee244d3fc26ba882d
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-24 09:47:27 +0530

    Sign In View Model

--- app/src/main/java/com/bphc/courseswap/viewmodels/SignIn.kt
+++ app/src/main/java/com/bphc/courseswap/viewmodels/SignIn.kt
@@ -2,9 +2,11 @@ package com.bphc.courseswap.viewmodels
 
 import android.app.Activity
 import android.content.Intent
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bphc.courseswap.R
+import com.bphc.courseswap.firebase.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -17,50 +19,21 @@ import com.google.firebase.ktx.Firebase
 
 class SignIn : ViewModel() {
 
-    private lateinit var mContext: Activity
-
-    private lateinit var googleSignInClient: GoogleSignInClient
-    var user: MutableLiveData<FirebaseUser>? = MutableLiveData()
-
-
-    fun getGoogleClient(context: Activity): GoogleSignInClient {
-
-        mContext = context
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(mContext.getString(R.string.server_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(mContext, gso)
-        return googleSignInClient
-    }
-
-    private fun getAuth(): FirebaseAuth {
-        return Firebase.auth
-    }
-
-    fun checkUser() {
-        setUser(getAuth().currentUser)
-    }
+    private var user: MutableLiveData<FirebaseUser>? = MutableLiveData()
 
     fun signOut() {
-        getAuth().signOut()
-        getGoogleClient(mContext).signOut().addOnCompleteListener(mContext) {
-            setUser(null)
-        }
+        Auth.auth().signOut()
     }
 
 
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        getAuth().signInWithCredential(credential)
-            .addOnCompleteListener(mContext) { task ->
+        Auth.auth().signInWithCredential(credential)
+            .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    setUser(getAuth().currentUser)
+                    user?.value = Auth.auth().currentUser
                 } else {
-                    setUser(null)
+                    user?.value = null
                 }
             }
     }
@@ -76,8 +49,13 @@ class SignIn : ViewModel() {
         }
     }
 
-    private fun setUser(firebaseUser: FirebaseUser?){
-        user?.value = firebaseUser
+    fun setUser() {
+        user?.value = null
+    }
+
+    fun user(): MutableLiveData<FirebaseUser>? {
+        user?.value = Auth.auth().currentUser
+        return user
     }
 
 }

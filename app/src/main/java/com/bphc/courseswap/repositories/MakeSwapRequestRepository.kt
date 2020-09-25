2e642cf4b48c199484054fc34644d797cf365f0a
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:49:23 +0530

    repo for posting user swap request on firestore

--- app/src/main/java/com/bphc/courseswap/repositories/MakeSwapRequestRepository.kt
+++ app/src/main/java/com/bphc/courseswap/repositories/MakeSwapRequestRepository.kt
@@ -1,2 +1,28 @@
 package com.bphc.courseswap.repositories
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.bphc.courseswap.firebase.Auth
+import com.bphc.courseswap.firebase.Database
+import com.bphc.courseswap.models.Course
+import com.bphc.courseswap.models.User
+import javax.inject.Inject
+
+class MakeSwapRequestRepository @Inject constructor() {
+
+    private val db = Database.instance()
+    private var isPosted: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun addCourse(course: Course): LiveData<Boolean> {
+
+            db.document("users/${Auth.userEmail}/swap/${course.assignedCourse}")
+                .set(course)
+                .addOnCompleteListener {
+                    isPosted.value = it.isSuccessful
+                }
+
+        return isPosted
+    }
+
+}

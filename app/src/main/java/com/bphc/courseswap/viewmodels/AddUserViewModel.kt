136a61c3319c24732c6673b6ca7bd49ff0e07df9
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:45:46 +0530

    view model for registering / setting user

--- app/src/main/java/com/bphc/courseswap/viewmodels/AddUserViewModel.kt
+++ app/src/main/java/com/bphc/courseswap/viewmodels/AddUserViewModel.kt
@@ -1,2 +1,17 @@
 package com.bphc.courseswap.viewmodels
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.bphc.courseswap.models.User
+import com.bphc.courseswap.repositories.AddUserRepository
+
+class AddUserViewModel @ViewModelInject constructor(
+    private val repository: AddUserRepository
+): ViewModel() {
+
+    fun addUser(user: User): LiveData<Boolean>{
+        return repository.addUser(user)
+    }
+
+}

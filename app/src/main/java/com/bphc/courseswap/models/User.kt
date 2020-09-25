2bf27fe6e91af5ee25825cf929c9080a763294e3
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:43:38 +0530

    model for user

--- app/src/main/java/com/bphc/courseswap/models/User.kt
+++ app/src/main/java/com/bphc/courseswap/models/User.kt
@@ -2,9 +2,7 @@ package com.bphc.courseswap.models
 
 data class User(
     val userEmail: String? = null,
-    val userPhoneNumber: String? = null,
-    val userCurrentCourse: String? = null,
-    val userDesiredCourse: String? = null )  {
+    val userPhoneNumber: String? = null)  {
 
 
 

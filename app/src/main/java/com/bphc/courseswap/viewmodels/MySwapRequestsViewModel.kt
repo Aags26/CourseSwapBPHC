33089b8e8da39ebfb28e2c05a47c3d7e3909d218
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:39:37 +0530

    view model

--- app/src/main/java/com/bphc/courseswap/viewmodels/MySwapRequestsViewModel.kt
+++ app/src/main/java/com/bphc/courseswap/viewmodels/MySwapRequestsViewModel.kt
@@ -1,2 +1,21 @@
 package com.bphc.courseswap.viewmodels
 
+import android.util.Log
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.bphc.courseswap.models.Course
+import com.bphc.courseswap.models.User
+import com.bphc.courseswap.repositories.MySwapRequestsRepository
+
+class MySwapRequestsViewModel @ViewModelInject constructor(
+    private val repository: MySwapRequestsRepository
+) : ViewModel() {
+
+    fun fetchMyRequests(user: User): LiveData<ArrayList<Course>>{
+        return repository.fetchMyRequests(user)
+    }
+
+
+}

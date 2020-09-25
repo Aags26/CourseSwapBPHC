1eba7338e280ec539409f3ba49165eae6cdb6e4c
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:41:27 +0530

    Contains list of user requests

--- app/src/main/java/com/bphc/courseswap/fragments/MySwapRequestsFragment.kt
+++ app/src/main/java/com/bphc/courseswap/fragments/MySwapRequestsFragment.kt
@@ -1,25 +1,38 @@
 package com.bphc.courseswap.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bphc.courseswap.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+import com.bphc.courseswap.adapters.MySwapRequestsAdapter
+import com.bphc.courseswap.firebase.Auth
+import com.bphc.courseswap.models.Course
+import com.bphc.courseswap.models.User
+import com.bphc.courseswap.viewmodels.MySwapRequestsViewModel
+import kotlinx.android.synthetic.main.fragment_my_swap_requests.*
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MySwapRequestsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class MySwapRequestsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+
+    private var myRequests: ArrayList<Course> = ArrayList()
+    private lateinit var mMySwapRequestViewModel: MySwapRequestsViewModel
+    private lateinit var recyclerView: RecyclerView
+    private val user: User = User(Auth.userEmail, Auth.userPhoneNumber)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,28 +42,51 @@ class MySwapRequestsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_swap_requests, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_my_swap_requests, container, false)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar_my_requests)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
+
+        mMySwapRequestViewModel =
+            ViewModelProvider(requireActivity()).get(MySwapRequestsViewModel::class.java)
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = MySwapRequestsAdapter(myRequests)
+
+        updateUI()
+
+        return view
+    }
+
+    private fun updateUI() {
+
+        mMySwapRequestViewModel.fetchMyRequests(user)
+            .observe(requireActivity(), {
+                if (it != null) {
+                    myRequests.clear()
+                    myRequests.addAll(it)
+                    recyclerView.adapter?.notifyDataSetChanged()
+                }
+            })
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MySwapRequestsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                MySwapRequestsFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MySwapRequestsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }

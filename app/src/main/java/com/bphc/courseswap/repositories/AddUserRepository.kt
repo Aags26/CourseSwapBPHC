629fb4a362810d1d038706f1df8dbc9439693b3d
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:46:38 +0530

    repo for registering / setting user into firestore

--- app/src/main/java/com/bphc/courseswap/repositories/AddUserRepository.kt
+++ app/src/main/java/com/bphc/courseswap/repositories/AddUserRepository.kt
@@ -1,2 +1,32 @@
 package com.bphc.courseswap.repositories
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.bphc.courseswap.firebase.Database
+import com.bphc.courseswap.models.User
+import javax.inject.Inject
+
+class AddUserRepository @Inject constructor() {
+
+    private val db = Database.instance()
+    private var isAdded: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun addUser(user: User): LiveData<Boolean> {
+
+        db.document("users/${user.userEmail}")
+            .set(user)
+            .addOnCompleteListener {
+                isAdded.value = if (it.isSuccessful) {
+                    Log.d("TAG", "Saved")
+                    true
+                } else {
+                    Log.w("TAG", it.exception)
+                    false
+                }
+            }
+        return isAdded
+
+    }
+
+}

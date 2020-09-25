a5b938104290180492d9c36402c65ef3d294da08
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:42:42 +0530

    singleton for firestore

--- app/src/main/java/com/bphc/courseswap/firebase/Database.kt
+++ app/src/main/java/com/bphc/courseswap/firebase/Database.kt
@@ -1,4 +1,13 @@
 package com.bphc.courseswap.firebase
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 object Database {
+
+    fun instance(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
 }

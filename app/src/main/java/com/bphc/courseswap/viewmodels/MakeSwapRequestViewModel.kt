aee9f35dba4087967211e4ea19c77c2b8152d6a9
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-25 18:48:55 +0530

    view model for making a swap request

--- app/src/main/java/com/bphc/courseswap/viewmodels/MakeSwapRequestViewModel.kt
+++ app/src/main/java/com/bphc/courseswap/viewmodels/MakeSwapRequestViewModel.kt
@@ -1,2 +1,18 @@
 package com.bphc.courseswap.viewmodels
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.bphc.courseswap.models.Course
+import com.bphc.courseswap.models.User
+import com.bphc.courseswap.repositories.MakeSwapRequestRepository
+
+class MakeSwapRequestViewModel @ViewModelInject constructor (
+    private val repository: MakeSwapRequestRepository
+) : ViewModel() {
+
+    fun addCourse(course: Course): LiveData<Boolean> {
+        return repository.addCourse(course)
+    }
+
+}

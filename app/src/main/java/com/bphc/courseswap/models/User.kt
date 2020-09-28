fb9390c1c48d553300aa1e063633c415ac30781f
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-29 00:55:52 +0530

    Added a new field for notification token

--- app/src/main/java/com/bphc/courseswap/models/User.kt
+++ app/src/main/java/com/bphc/courseswap/models/User.kt
@@ -2,7 +2,8 @@ package com.bphc.courseswap.models
 
 data class User(
     val userEmail: String? = null,
-    val userPhoneNumber: String? = null)  {
+    val userPhoneNumber: String? = null,
+    val userNotificationToken: String? = null)  {
 
 
 

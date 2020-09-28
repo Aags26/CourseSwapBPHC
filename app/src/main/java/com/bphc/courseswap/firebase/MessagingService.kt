6784c1b65b7eafd548d96467759674a4382334bd
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-29 00:57:31 +0530

    FCM enabler

--- app/src/main/java/com/bphc/courseswap/firebase/MessagingService.kt
+++ app/src/main/java/com/bphc/courseswap/firebase/MessagingService.kt
@@ -1,2 +1,53 @@
 package com.bphc.courseswap.firebase
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.bphc.courseswap.R
+import com.bphc.courseswap.app.Constants
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService() : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Log.d("TOKEN", p0);
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        Log.d("FCM", p0.data.toString())
+        sendNotification(p0.notification)
+    }
+
+    private fun sendNotification(message: RemoteMessage.Notification?) {
+
+        val channelId = Constants.NOTIFICATION_CHANNEL_ID
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setWhen(System.currentTimeMillis())
+            .setContentTitle(message?.title)
+            .setContentText(message?.body)
+            .setAutoCancel(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Notification",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 , notificationBuilder.build())
+    }
+
+}

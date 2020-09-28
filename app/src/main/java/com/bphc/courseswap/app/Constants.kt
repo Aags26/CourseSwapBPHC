0e61e7034062672c935b6e157264cbb31a4a5e29
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-29 00:49:10 +0530

    App Constants associated with data payload of a notification message

--- app/src/main/java/com/bphc/courseswap/app/Constants.kt
+++ app/src/main/java/com/bphc/courseswap/app/Constants.kt
@@ -1,4 +1,18 @@
 package com.bphc.courseswap.app
 
 class Constants {
+
+    companion object {
+        /* const val BASE_URL = "https://fcm.googleapis.com/"
+        const val CONTENT_TYPE = "application/json"
+        const val SEND_NOTIFICATION = "fcm/send" */
+
+        const val NOTIFICATION_CHANNEL_ID = "com.bphc.courseswap"
+
+        const val USER_EMAIL = "USER_EMAIL"
+        const val USER_PHONE = "USER_PHONE"
+        const val USER_ASSIGNED = "USER_ASSIGNED"
+        const val USER_DESIRED = "USER_DESIRED"
+    }
+
 }

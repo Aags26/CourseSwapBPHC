ff613d3f852ef322609d99c375886a884d695a4b
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-09-29 21:24:19 +0530

    Updated

--- app/src/main/java/com/bphc/courseswap/app/Constants.kt
+++ app/src/main/java/com/bphc/courseswap/app/Constants.kt
@@ -3,9 +3,6 @@ package com.bphc.courseswap.app
 class Constants {
 
     companion object {
-        /* const val BASE_URL = "https://fcm.googleapis.com/"
-        const val CONTENT_TYPE = "application/json"
-        const val SEND_NOTIFICATION = "fcm/send" */
 
         const val NOTIFICATION_CHANNEL_ID = "com.bphc.courseswap"
 

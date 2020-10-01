3419e9f1451f9e82b507887e814dd45062f203c8
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-02 00:14:03 +0530

    Added an extra field

--- app/src/main/java/com/bphc/courseswap/models/Course.kt
+++ app/src/main/java/com/bphc/courseswap/models/Course.kt
@@ -2,7 +2,8 @@ package com.bphc.courseswap.models
 
 data class Course(
     val assignedCourse: String? = null,
-    val desiredCourse: String? = null
+    val desiredCourse: String? = null,
+    val match: String? = "none"
 )  {
 
 }

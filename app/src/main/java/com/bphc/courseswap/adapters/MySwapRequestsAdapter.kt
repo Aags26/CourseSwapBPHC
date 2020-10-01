3ffa1b91a910fc4a32d61e7976629aeea616b246
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-02 00:17:02 +0530

    Updated

--- app/src/main/java/com/bphc/courseswap/adapters/MySwapRequestsAdapter.kt
+++ app/src/main/java/com/bphc/courseswap/adapters/MySwapRequestsAdapter.kt
@@ -1,9 +1,9 @@
 package com.bphc.courseswap.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bphc.courseswap.R
@@ -11,17 +11,38 @@ import com.bphc.courseswap.models.Course
 import kotlinx.android.synthetic.main.my_swap_request.view.*
 
 class MySwapRequestsAdapter(
-    private val mySwapRequests: ArrayList<Course>
+    private val listener: OnItemClickListener
 ) : RecyclerView.Adapter<MySwapRequestsAdapter.MySwapRequestsViewHolder>() {
 
-    class MySwapRequestsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var mySwapRequests: ArrayList<Course> = ArrayList()
+
+    fun setList(myRequests: ArrayList<Course>) {
+        mySwapRequests = myRequests
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    inner class MySwapRequestsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val assignedCourse: TextView = itemView.my_assigned_course
         val desiredCourse: TextView = itemView.my_desired_course
+        private val delete: ImageView = itemView.delete
+
+        init {
+            delete.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onItemClick(position)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MySwapRequestsViewHolder {
 
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.my_swap_request, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.my_swap_request, parent, false)
         return MySwapRequestsViewHolder(itemView)
     }
 
@@ -29,8 +50,6 @@ class MySwapRequestsAdapter(
         val myRequest = mySwapRequests[position]
         holder.assignedCourse.text = myRequest.assignedCourse
         holder.desiredCourse.text = myRequest.desiredCourse
-
-        //Log.d("COURSES", myRequest.assignedCourse + myRequest.desiredCourse)
     }
 
     override fun getItemCount() = mySwapRequests.size

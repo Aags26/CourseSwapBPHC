a78af276c66538e4c4d53bc1fec027ff39e50f26
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-02 16:18:27 +0530

    Shifted to Activity result contracts

--- app/src/main/java/com/bphc/courseswap/fragments/SignInFragment.kt
+++ app/src/main/java/com/bphc/courseswap/fragments/SignInFragment.kt
@@ -2,45 +2,33 @@ package com.bphc.courseswap.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import com.bphc.courseswap.R
 import com.bphc.courseswap.firebase.Auth
 import com.bphc.courseswap.viewmodels.SignIn
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_sign_in.*
-
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 
 class SignInFragment : Fragment(), View.OnClickListener {
 
-    private lateinit var mSignInViewModel: SignIn
+    private val mSignInViewModel: SignIn by viewModels()
     private lateinit var googleSignInClient: GoogleSignInClient
-    var navController: NavController? = null
+    private var navController: NavController? = null
 
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,8 +40,6 @@ class SignInFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mSignInViewModel = ViewModelProvider(this).get(SignIn::class.java)
-
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.server_client_id))
             .requestEmail()
@@ -64,10 +50,18 @@ class SignInFragment : Fragment(), View.OnClickListener {
         signInButton.setOnClickListener(this)
         signOutButton.setOnClickListener(this)
 
+        mLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            mSignInViewModel.getUserAccount(it.data)
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(requireActivity())
+        mSignInViewModel.setUser(account)
         updateUI()
     }
 
@@ -79,19 +73,13 @@ class SignInFragment : Fragment(), View.OnClickListener {
     }
 
     private fun signIn() {
-        startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (RC_SIGN_IN == requestCode)
-            mSignInViewModel.getUserAccount(data)
+        mLauncher.launch(googleSignInClient.signInIntent)
     }
 
     private fun signOut() {
-        mSignInViewModel.signOut()
+        mSignInViewModel.firebaseSignOut()
         googleSignInClient.signOut().addOnCompleteListener(requireActivity()) {
-            mSignInViewModel.setUser()
+            mSignInViewModel.googleSignOut()
         }
     }
 
@@ -99,14 +87,12 @@ class SignInFragment : Fragment(), View.OnClickListener {
 
         mSignInViewModel.user()?.observe(requireActivity(), { user ->
             if (user != null) {
-
                 if (user.email?.isNotEmpty()!!) {
                     Auth.userEmail = user.email.toString()
                     Toast.makeText(context, user.email, Toast.LENGTH_SHORT).show()
                     navController = view?.let { Navigation.findNavController(it) }
                     navController!!.navigate(R.id.action_signInFragment_to_phoneAuthFragment)
-                } else
-                    signOut()
+                }
             } else {
                 Toast.makeText(context, "null", Toast.LENGTH_SHORT).show()
             }
@@ -118,12 +104,5 @@ class SignInFragment : Fragment(), View.OnClickListener {
 
         private const val RC_SIGN_IN = 9001
 
-        fun newInstance(param1: String, param2: String) =
-            SignInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }

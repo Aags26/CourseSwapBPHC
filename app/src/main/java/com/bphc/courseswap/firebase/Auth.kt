800a01ace695fd11dcd4ad9a04510da740551674
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-02 16:17:03 +0530

    Updated

--- app/src/main/java/com/bphc/courseswap/firebase/Auth.kt
+++ app/src/main/java/com/bphc/courseswap/firebase/Auth.kt
@@ -1,19 +1,8 @@
 package com.bphc.courseswap.firebase
 
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
 object Auth {
 
     lateinit var userEmail: String
     lateinit var userPhoneNumber: String
-
-
-    fun auth() : FirebaseAuth {
-        return Firebase.auth
-    }
-
-
 
 }

bc9b31f0f3ba0bb529995218912171630fa4d07b
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-02 16:17:15 +0530

    Updated

--- app/src/main/java/com/bphc/courseswap/firebase/MessagingService.kt
+++ app/src/main/java/com/bphc/courseswap/firebase/MessagingService.kt
@@ -16,12 +16,14 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 
-class MessagingService() : FirebaseMessagingService() {
+class MessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(p0: String) {
         super.onNewToken(p0)
         Log.d("TOKEN", p0)
     }
+
+
 
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
